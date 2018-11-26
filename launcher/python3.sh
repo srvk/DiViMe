06450f26fef90926812196c4f96996b4981c67e7
Junghan <junghan0305@gmail.com>
2018-11-26 06:47:04 +0000

    Add python3 env
     - separate bootstrapping from Vagrant file
     - make conda env instead of downloading new miniconda

---
+++ launcher/python3.sh
@@ -0,0 +1,13 @@
+# activate divime environment to use python 3.6.5
+source activate divime
+
+# list of libararies installed can be found with
+# conda list 
+# to install new packages, edit environment.yml file
+
+
+# sample usage
+# python [some_file] ...
+
+# to switch back to python 2 
+source deactivate
