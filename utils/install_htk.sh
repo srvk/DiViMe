9fe300276efb19514420a19af1108f01b52d1c53
alecristia <alecristia@gmail.com>
2018-11-26 16:34:56 -0500

    added routine to install htk after provisioning

---
+++ utils/install_htk.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# install HTK after the fact
+
+# optionally Install HTK (without it, some other tools will not work)
+# the idea is to make users independently download HTK installer since
+# we cannot redistribute
+cd /home/${user}
+if [ -f /vagrant/HTK.tar.gz ]; then
+    if [[ ! -d htk ]]; then
+    cd /home/${user}/repos/
+    su ${user} -c "tar zxf /vagrant/HTK.tar.gz"
+    cd htk
+    ./configure --without-x --disable-hslab
+    sed -i "s/        /\t/g" HLMTools/Makefile # fix bad Makefile
+    make all
+    make install
+    fi
+fi
+
+
