8c893d04f1f199e11c8fc1a7a20a3468a440a073
Junghan <junghan0305@gmail.com>
2018-11-27 14:45:56 -0500

    move config files to conf/

---
+++ conf/update.sh
@@ -0,0 +1,26 @@
+
+if grep --quiet vagrant /etc/passwd
+then
+    user="vagrant"
+else
+    user="ubuntu"
+fi
+
+echo "Check git updates"
+#############################################
+# Get OpenSAT and all the tools
+# change version tag as necessary for each tools
+cd /home/${user}/repos
+(cd "OpenSAT"; git checkout master; git pull)
+# (cd "OpenSAT"; git checkout master; git pull; git checkout v2.0)
+(cd "ib_diarization_toolkit"; git checkout master; git pull)
+# (cd "ib_diarization_toolkit" ; git checkout master; git pull; git checkout v2.0)
+(cd "dscore" ; git checkout master; git pull)
+# (cd "dscore" ;git checkout master;  git pull; git checkout v2.0)
+(cd "Yunitator" ;git checkout master;  git pull)
+# (cd "Yunitator" ; git checkout master; git pull; git checkout v2.0)
+(cd "vcm"; git checkout master; git pull)
+# (cd "vcm"; git checkout master; git pull; git checkout v2.0)
+(cd "To-Combo-SAD" ; git checkout master; git pull)
+# (cd "To-Combo-SAD" ; git checkout master; git pull; git checkout v2.0)
+#############################################
