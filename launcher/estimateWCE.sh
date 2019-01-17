84f4cfb1a741e390a1149f2542774b66e045024a
orasanen <okko.rasanen@aalto.fi>
2019-01-17 08:16:24 +0000

    updated estimateWCE.sh to match WCE out-of-the-box requirements

--- launcher/estimateWCE.sh
+++ launcher/estimateWCE.sh
@@ -12,7 +12,7 @@ OUTPUTFILE=$2
 fi
 
 MATPATH="/usr/local/MATLAB/MATLAB_Runtime/v93/"
-MODEL_FINAL="~/repos/WCE_VM/models/model_final.mat"
+MODEL_FINAL="~/repos/WCE_VM/models/model_default.mat"
 
 #python ... write python code that averages all the results of the speakers
 ~/repos/WCE_VM/run_WCEestimate.sh ${MATPATH} ${FILES_TEST} ${MODEL_FINAL} ${OUTPUTFILE}
