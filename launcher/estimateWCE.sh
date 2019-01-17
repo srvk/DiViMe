1e2d4bc2fe807a72872d546d70e3dc3a16ff3244
orasanen <okko.rasanen@aalto.fi>
2019-01-17 11:34:51 +0000

    updated estimateWCE.sh to be compatible with DiViMe input data syntax

--- launcher/estimateWCE.sh
+++ launcher/estimateWCE.sh
@@ -14,7 +14,8 @@ exit 1
 fi
 
 
-
+# If input is a directory, find all .wavs in it and write to a file to be used as input
+# to the WCE tool
 if [[ -d $1 ]]; then
   find $1*.wav > ~/repos/WCE_VM/file_list.txt
   FILES_TEST=~/repos/WCE_VM/file_list.txt
@@ -26,6 +27,7 @@ elif [ ! -d "$1" ] && [ ! -f $1 ]; then
 fi
 
 
+# Replace DiViMe way of addressing data-folder with an absolute path
 if [ -n "$2" ]; then
   OUTPUTFILE=$2
   OUTPUTFILE=${OUTPUTFILE/"data/"/"/vagrant/data/"}
@@ -40,9 +42,19 @@ else
   MODEL="~/repos/WCE_VM/models/model_default.mat"
 fi
 
+if [ ! -d "/vagrant/data/WCE_VM_TEMP/" ]; then
+  mkdir -p /vagrant/data/WCE_VM_TEMP/
+fi
+
 MATPATH="/usr/local/MATLAB/MATLAB_Runtime/v93/"
 
 echo "Running WCE module (this might take a while...)"
 ~/repos/WCE_VM/run_WCEestimate.sh ${MATPATH} ${FILES_TEST} ${MODEL} ${OUTPUTFILE} > /vagrant/data/WCE_VM_TEMP/WCE_process_log.log
+
+# Combine filenames and output counts into one file
+paste -d ', ' $FILES_TEST $OUTPUTFILE > /vagrant/data/WCE_VM_TEMP/tempout.txt
+cp /vagrant/data/WCE_VM_TEMP/tempout.txt $OUTPUTFILE
+sed -i 's+/vagrant/data/+data/+g' $OUTPUTFILE
+
 
 echo "WCE processing complete. Wrote output to $OUTPUTFILE"
