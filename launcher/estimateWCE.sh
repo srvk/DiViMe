23b56c14a011465e2e4f107f4d6f25d2328d575b
orasanen <okko.rasanen@aalto.fi>
2019-01-17 09:17:23 +0000

    updated estimateWCE.sh to support directories

--- launcher/estimateWCE.sh
+++ launcher/estimateWCE.sh
@@ -4,15 +4,37 @@ SCRIPT_DIR=$(dirname "$0")
 
 FILES_TEST=$1
 
+if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]  && [ "$#" -ne 3 ]  ; then
+echo "Usage: $0 <list_of_filenames.txt or audio folder> <output_file.csv> (optional) <WCE modelfile.mat> (optional)"
+echo "Example:"
+echo "$0 /vagrant/data/my_audiodata/ ~/my_WCE_output.txt"
+echo "or"
+echo "$0 /vagrant/data/list_of_audiofiles.txt ~/my_WCE_output.txt"
+exit 1
+fi
+
+if [[ -d $1 ]]; then
+  find $1*.wav > ~/repos/WCE_VM/file_list.txt
+  FILES_TEST=~/repos/WCE_VM/file_list.txt
+fi
+
 
 if [ -n "$2" ]; then
-OUTPUTFILE = '/vagrant/repos/WCE_VM/outputs/default_output.csv'
+  OUTPUTFILE=$2
 else
-OUTPUTFILE=$2
+  OUTPUTFILE='/vagrant/repos/WCE_VM/outputs/default_output.csv'
+fi
+
+if [ -n "$3" ]; then
+  MODEL=$3
+else
+  MODEL="~/repos/WCE_VM/models/model_default.mat"
 fi
 
 MATPATH="/usr/local/MATLAB/MATLAB_Runtime/v93/"
-MODEL_FINAL="~/repos/WCE_VM/models/model_default.mat"
 
-#python ... write python code that averages all the results of the speakers
-~/repos/WCE_VM/run_WCEestimate.sh ${MATPATH} ${FILES_TEST} ${MODEL_FINAL} ${OUTPUTFILE}
+echo $MODEL
+echo $OUTPUTFILE
+
+
+~/repos/WCE_VM/run_WCEestimate.sh ${MATPATH} ${FILES_TEST} ${MODEL} ${OUTPUTFILE}
