6ffc331dd6e1c63d9591fc107f4bd4563d775180
orasanen <okko.rasanen@aalto.fi>
2019-01-17 11:07:32 +0000

    updated estimateWCE to support DiViMe data structure

--- launcher/estimateWCE.sh
+++ launcher/estimateWCE.sh
@@ -13,16 +13,25 @@ echo "$0 /vagrant/data/list_of_audiofiles.txt ~/my_WCE_output.txt"
 exit 1
 fi
 
+
+
 if [[ -d $1 ]]; then
   find $1*.wav > ~/repos/WCE_VM/file_list.txt
   FILES_TEST=~/repos/WCE_VM/file_list.txt
+elif [ ! -d "$1" ] && [ ! -f $1 ]; then
+  if [[ -d /vagrant/$1 ]]; then
+    find /vagrant/$1*.wav > ~/repos/WCE_VM/file_list.txt
+    FILES_TEST=~/repos/WCE_VM/file_list.txt
+  fi
 fi
 
 
 if [ -n "$2" ]; then
   OUTPUTFILE=$2
+  OUTPUTFILE=${OUTPUTFILE/"data/"/"/vagrant/data/"}
+  OUTPUTFILE=${OUTPUTFILE/"/vagrant//vagrant/"/"/vagrant/"}
 else
-  OUTPUTFILE='/vagrant/repos/WCE_VM/outputs/default_output.csv'
+  OUTPUTFILE='~/repos/WCE_VM/outputs/default_output.csv'
 fi
 
 if [ -n "$3" ]; then
@@ -33,8 +42,7 @@ fi
 
 MATPATH="/usr/local/MATLAB/MATLAB_Runtime/v93/"
 
-echo $MODEL
-echo $OUTPUTFILE
+echo "Running WCE module (this might take a while...)"
+~/repos/WCE_VM/run_WCEestimate.sh ${MATPATH} ${FILES_TEST} ${MODEL} ${OUTPUTFILE} > ~/repos/WCE_VM/process_log.log
 
-
-~/repos/WCE_VM/run_WCEestimate.sh ${MATPATH} ${FILES_TEST} ${MODEL} ${OUTPUTFILE}
+echo "WCE processing complete. Wrote output to $OUTPUTFILE"
