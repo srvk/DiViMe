8ffe2e7c6ecc6eefdcff2237093b733a2fe7539d
MarvinLvn <marvin.lavechin@etu.upmc.fr>
2019-02-25 18:35:30 +0100

    Diartk produces empty rttm when the SAD is empty (it was producing nothing before)

--- launcher/diartk.sh
+++ launcher/diartk.sh
@@ -114,6 +114,9 @@ for fin in `ls ${audio_dir}/*.wav`; do
         # print results
         #cat $workdir/$basename.out
         cp $workdir/$basename.rttm ${audio_dir}/diartk_${sys}_${basename}.rttm
+    else
+        # Create empty output file
+        touch ${audio_dir}/diartk_${sys}_${basename}.rttm
     fi
 done
 
