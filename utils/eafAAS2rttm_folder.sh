34c65e8dc912c4b972defceafb490a393afd7e5f
alecristia <alecristia@gmail.com>
2019-07-15 17:41:32 -0400

    addressing #135 in progress

--- utils/eafAAS2rttm_folder.sh
+++ utils/eafAAS2rttm_folder.sh
@@ -2,6 +2,8 @@
 #
 # Shell script to convert all .eaf files following the ACLEW Annotation Scheme inside a folder to .rttm format
 # 
-for j in ${1}/*.eaf
-    do elan2rttm.py $j #generate the basic rttms
+for j in ${1}/*.eaf; do
+    r=`echo $j | sed "s/.eaf/rttm/"`
+    elan2rttm.py -i $j -o $r
+#generate the basic rttms
 done
